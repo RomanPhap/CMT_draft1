9284a838535906f47b343e5c7052981a32089b34
RomanPhap <romanphap@gmail.com>
2018-04-08 15:34:40 +0200

    minor updates

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-Website in developpement.
+# Caritas My Tho Web Site
+
+
+
+### draft 01 in dev 08/04/2018
+
+Website in developpement for Caritas My Tho.
+
 
 Basic website structure w/ static html pages, bootstrap CSS and a custom CSS sheet.
+
+TODO : 
+* jquery structure for News Pages
+* Carousel front-end for news Pages
+* Admin Page
+* Ajax web queries
---
+++ js/news.js
@@ -0,0 +1,11 @@
+/*
+	JS document by Roman
+*/
+
+/*
+console.log("yo this has been triggered");
+document.getElementById("news_title").innerHTML = "News #2 Title";
+document.getElementById("news_subtitle").innerHTML = "News #2  subtitle";
+document.getElementById("news_text").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt impedit est voluptatem doloremque architecto corporis suscipit quidem ratione! Quis laborum nam optio dolorem doloremque ex nobis quibusdam ad quo dolores? News2";
+document.getElementById("news_image").src = "../images/Caritas-Vietnam.gif";
+*/
