efb693ff19021324ed60d71ae112cd72b2d5088b
RomanPhap <romanphap@gmail.com>
2018-04-08 17:35:18 +0200

    added admin draft page + changes in news loading
    news loading uses first function loadNews() and then uses changeNewsCOntent(news_id) etc

--- js/news.js
+++ js/news.js
@@ -1,10 +1,43 @@
 /*
 	JS document by Roman
 */
+function loadNews(){
+	changeNewsContent(2);
+}
 
 
-console.log("yo this has been triggered");
-document.getElementById("news_title").innerHTML = "News #2 Title";
-document.getElementById("news_subtitle").innerHTML = "News #2  subtitle";
-document.getElementById("news_text").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt impedit est voluptatem doloremque architecto corporis suscipit quidem ratione! Quis laborum nam optio dolorem doloremque ex nobis quibusdam ad quo dolores? News2";
-document.getElementById("news_image").src = "../images/Caritas-Vietnam.gif";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//		handle story changing
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function changeNewsContent(news_id){
+
+	//news id var should be integer type
+
+	console.log("-- yo news number "+news_id+" has been triggered");
+
+	document.getElementById("news_title").innerHTML = getNewsTitle(news_id);
+	document.getElementById("news_subtitle").innerHTML = getNewsSubtitle(news_id);
+	document.getElementById("news_text").innerHTML = getNewsText(news_id);
+	document.getElementById("news_image").src = getNewsImagePath(news_id);
+
+	console.log("news number "+news_id+" has been loaded");
+}
+
+// we may use here JSON parsing
+function getNewsTitle(news_id){
+	return "News #"+news_id+" Title";
+}
+
+function getNewsSubtitle(news_id){
+	return "News #"+news_id+" subtitle";
+}
+
+function getNewsText(news_id){
+	return "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt impedit est voluptatem doloremque architecto corporis suscipit quidem ratione! Quis laborum nam optio dolorem doloremque ex nobis quibusdam ad quo dolores?";
+}
+
+function getNewsImagePath(news_id){
+	return "../images/news/"+news_id+".jpg";
+}
+
+
